e4799a5df0845f5e1adf9e2cec494a0b551cde19
Ahmed Abdien <ahmed.g.abdien@gmail.com>
2023-06-03 13:46:34 +0300

    uniqueName image

--- src/components/UploadWidget.jsx
+++ src/components/UploadWidget.jsx
@@ -1,23 +1,40 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 
 const UploadWidget = ({ name }) => {
+
+    const [imageUrl, setImageUrl] = useState('');
     const cloudinaryRef = useRef()
     const widgetRef = useRef()
+    const newDate = new Date()
+    const date = newDate.getTime()
+    const uniqueName = name + date
+
+    const handleImageUpload = (_, result) => {
+        if (result && result.event === 'success') {
+            setImageUrl(result.info.url);
+        }
+        // else {
+        //     console.log('Error uploading image:', result);
+        // }
+    };
 
     useEffect(() => {
         cloudinaryRef.current = window.cloudinary
         widgetRef.current = cloudinaryRef.current.createUploadWidget({
             cloudName: "dcmthd8bn",
             uploadPreset: "utmds9zl",
-            publicId: name,
-        }, (error, result) => console.log(error))
+            publicId: uniqueName,
+            sources: ['local', 'url', 'image_search'],
+        }, handleImageUpload)
     }, [])
 
-    return (
+    return (<>
         <button onClick={() => widgetRef.current.open()}>
             upload
         </button>
-    )
+        <br />
+        {imageUrl && <img src={imageUrl} alt="Uploaded Image" />}
+    </>)
 }
 
 export default UploadWidget
