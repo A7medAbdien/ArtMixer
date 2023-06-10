94b75a5c8bfe242ffe0cf27a36e35a3bbc32d355
Ahmed Abdien <ahmed.g.abdien@gmail.com>
2023-06-10 21:39:44 +0300

    WRoom iframe

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -5,10 +5,14 @@ import Model from './Model'
 import GRoom from './GRoom'
 import BRoom from './BRoom'
 import WRoom from './WRoom'
+import { Leva } from 'leva'
 
 export default function Experience() {
     return <>
-        <Canvas>
+        {/* <Leva hidden /> */}
+        <Canvas
+            flat dpr={[1, 1.5]} camera={{ fov: 95, position: [0, 0, 1] }}
+        >
             <Perf position="top-left" />
             <fog attach="fog" args={['#191920', 0, 15]} />
             <OrbitControls makeDefault />
--- src/WRoom.jsx
+++ src/WRoom.jsx
@@ -1,19 +1,34 @@
-import { Center, OrbitControls, useGLTF, useTexture } from '@react-three/drei'
+import * as THREE from 'three'
+import { Center, Html, Image, useGLTF, useTexture } from '@react-three/drei'
+import { useControls } from 'leva'
 
 
 export default function WRoom() {
 
 
+    const { position, scale } = useControls('TF',
+        {
+            position:
+            {
+                value: { x: -0.06879992675781248, y: 1.1583999862670897 },
+                step: 0.001,
+                joystick: 'invertY'
+            },
+            scale:
+            {
+                value: { x: 53.4, y: -52.5 },
+                step: 0.1,
+                joystick: 'invertY'
+            },
+
+        })
+
+    const url = "https://images.pexels.com/photos/17137556/pexels-photo-17137556/free-photo-of-wood-animal-tree-lizard.jpeg"
     const { nodes } = useGLTF('./model//WRoom/WRoom.glb')
-    // const { nodes: jnodes } = useGLTF('./model/WRoom/Vase.glb')
     console.log(nodes);
-    // console.log(nodes);
-    // console.log(jnodes);
 
     const bakedTexture = useTexture('./model/WRoom/WRoom.jpg')
-    // const jarsTexture = useTexture('./model/WRoom/Vase.jpg')
     bakedTexture.flipY = false
-    // jarsTexture.flipY = false
 
     return <>
         <color args={['#201919']} attach="background" />
@@ -21,11 +36,27 @@ export default function WRoom() {
 
         <Center>
 
-            <mesh geometry={nodes.baked.geometry}>
+            <mesh
+                geometry={nodes.baked.geometry}>
                 <meshBasicMaterial map={bakedTexture} />
             </mesh>
-            <mesh geometry={nodes.image.geometry}>
-                <meshBasicMaterial />
+            <mesh
+                geometry={nodes.image.geometry}>
+
+                <Html
+                    transform
+                    wrapperClass='htmlScreen'
+                    distanceFactor={1.17}
+                    position={[position.x, position.y, 0.5]}
+                >
+                    <iframe
+                        style={{
+                            width: 1048 + scale.x,
+                            height: 670 + scale.y,
+                        }}
+                        src='https://fluid-threejs.netlify.app/' />
+                </Html>
+                <meshBasicMaterial transparent opacity={0} />
             </mesh>
 
 
@@ -38,6 +69,6 @@ export default function WRoom() {
                 count={40}
             /> */}
 
-        </Center>
+        </Center >
     </>
 }
