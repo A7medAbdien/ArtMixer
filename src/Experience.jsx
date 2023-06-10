17bbc4eb667cb834bfe70be30fc732a1ea136d42
Ahmed Abdien <ahmed.g.abdien@gmail.com>
2023-06-11 00:08:19 +0300

    GRoom ifreame, Colab does not work as iFrame 😩

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -12,7 +12,7 @@ export default function Experience() {
     return <>
         {/* <Leva hidden /> */}
         <Canvas
-            flat dpr={[1, 1.5]} camera={{ fov: 95, position: [0, 0, 1] }}
+            flat dpr={[1, 1.5]} camera={{ fov: 95, position: [0, 0, 0.25] }}
         >
             <Perf position="top-left" />
             <fog attach="fog" args={['#191920', 0, 15]} />
@@ -25,8 +25,8 @@ export default function Experience() {
             </mesh> */}
 
             {/* <Model /> */}
-            {/* <GRoom /> */}
-            <BRoom />
+            <GRoom />
+            {/* <BRoom /> */}
             {/* <WRoom /> */}
 
         </Canvas>
--- src/components/Rooms/GRoom.jsx
+++ src/components/Rooms/GRoom.jsx
@@ -1,14 +1,31 @@
-import { Center, OrbitControls, useGLTF, useTexture } from '@react-three/drei'
+import { Center, Html, useGLTF, useTexture } from '@react-three/drei'
+import { useControls } from 'leva';
 
 
 export default function GRoom() {
 
+    const { position, scale } = useControls('GRoom',
+        {
+            position:
+            {
+                value: { x: 0.2056, y: 1.123 },
+                step: 0.001,
+                joystick: 'invertY'
+            },
+            // {"position":{"x":0.205600048828125,"y":1.123200004577637}}
+            scale:
+            {
+                value: { x: -349.2, y: -246.0 },
+                step: 0.1,
+                joystick: 'invertY'
+            },
+            // {"scale":{"x":-349.20000000000016,"y":-246.0599983215332}}
+        })
+
 
     const { nodes } = useGLTF('./model//GRoom/GRoom2.glb')
     const { nodes: jnodes } = useGLTF('./model/GRoom/Vase.glb')
-    // console.log(nodes);
-    // console.log(nodes);
-    console.log(jnodes);
+    console.log(nodes);
 
     const bakedTexture = useTexture('./model/GRoom/GRoom2.jpg')
     const jarsTexture = useTexture('./model/GRoom/Vase.jpg')
@@ -24,12 +41,29 @@ export default function GRoom() {
             <mesh geometry={jnodes.Modern_White_Vase001.geometry}>
                 <meshBasicMaterial map={jarsTexture} />
             </mesh>
+
             <mesh geometry={nodes.baked.geometry}>
                 <meshBasicMaterial map={bakedTexture} />
             </mesh>
-            {/* <mesh geometry={nodes.baked.geometry}>
-                <meshBasicMaterial map={bakedTexture} />
-            </mesh> */}
+
+            <mesh
+                geometry={nodes.image.geometry}>
+
+                <Html
+                    transform
+                    wrapperClass='htmlScreen'
+                    distanceFactor={1.17}
+                    position={[position.x, position.y, 0.5]}
+                >
+                    <iframe
+                        style={{
+                            width: 1048 + scale.x,
+                            height: 670 + scale.y,
+                        }}
+                        src='https://fluid-threejs.netlify.app/' />
+                </Html>
+                <meshBasicMaterial transparent opacity={0} />
+            </mesh>
 
 
 
