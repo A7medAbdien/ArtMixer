631dc6e3ef614e1871f8392c91fdf894584e038c
Ahmed Abdien <ahmed.g.abdien@gmail.com>
2023-06-13 10:35:27 +0300

    GRoom - MassageBubble compo

--- src/components/Rooms/GRoom.jsx
+++ src/components/Rooms/GRoom.jsx
@@ -9,7 +9,7 @@ export default function GRoom() {
         {
             position:
             {
-                value: { x: -1.46, y: 1.75 },
+                value: { x: -1.33, y: 1.645 },
                 // value: { x: 0, y: 0 },
                 step: 0.01,
                 joystick: 'invertY'
@@ -29,6 +29,7 @@ export default function GRoom() {
     const images = [
         { name: "ImageB", position: [0.227, 1.13, 0.4], args: [2.2, 1.4], url: '10.mp4' },
     ]
+
     const textPosition = [position.x, position.y, 0.4]
     const { nodes } = useGLTF('./model//GRoom/GRoom2.glb')
     const { nodes: vaseNodes } = useGLTF('./model/GRoom/Vase.glb')
@@ -51,15 +52,14 @@ export default function GRoom() {
                 </mesh>
 
                 <Frames basePOV={[0, -0.33, 1.23]} bigImageFocus={1.05} bigImageFocusX={-0.31} Children={Frame} images={images} pointerMissDeactivate />
-                <HoverableFrame colorNotScale>
-                    <Text color={"green"} maxWidth={1} position={textPosition} fontSize={0.05} onClick={() => console.log("hi")}>
-                        1. Open ArtMixer Notebook
-                    </Text>
-                </HoverableFrame>
+
+                <MassageBubble textPosition={textPosition} />
+
             </Center>
         </group>
     </>
 }
+
 
 const Frame = ({ name, position, args, url }) => {
 
@@ -70,7 +70,29 @@ const Frame = ({ name, position, args, url }) => {
                 <VideoFrame url={url} />
             </mesh>
         </HoverableFrame>
+    </>
+}
 
 
+const MassageBubble = ({ textPosition }) => {
+
+    return <>
+        <Html
+            transform
+            scale={0.5}
+            distanceFactor={1.17}
+            position={textPosition}>
+            <div className="talk-bubble tri-right round right-top">
+                <div className="talktext">
+                    <p>
+                        <ol>
+                            <li>Open <a href="https://www.example.com"> ArtMixer Notebook</a></li>
+                            <li>On top left corner, open "Runtime"</li>
+                            <li>Choose "Run All"</li>
+                        </ol>
+                    </p>
+                </div>
+            </div>
+        </Html>
     </>
 }
