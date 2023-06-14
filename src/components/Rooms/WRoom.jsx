57d3b2dba46e372f37910227720705cb581cf978
Ahmed Abdien <ahmed.g.abdien@gmail.com>
2023-06-14 17:55:19 +0300

    WRoom - Show iframe when active - ER no back

--- src/components/Rooms/WRoom.jsx
+++ src/components/Rooms/WRoom.jsx
@@ -1,6 +1,9 @@
 import * as THREE from 'three'
 import { Center, Html, Image, useGLTF, useTexture } from '@react-three/drei'
 import { useControls } from 'leva'
+import { useState } from 'react';
+import { useRoute } from 'wouter';
+import { useEffect } from 'react';
 
 
 export default function WRoom() {
@@ -23,7 +26,23 @@ export default function WRoom() {
 
         })
 
-    const url = "https://images.pexels.com/photos/17137556/pexels-photo-17137556/free-photo-of-wood-animal-tree-lizard.jpeg"
+
+    const url = 'https://fluid-threejs.netlify.app/'
+    const image = { name: "WhiteRoom", position: [-0.06879992675781248, 1.1583999862670897, 0.4], args: [53.4, -52.5], url: url, waitingTime: 95000 }
+
+    const [show, setShow] = useState(false);
+    const [, params] = useRoute('/:id')
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            (params.id == image.name) && setShow(true);
+        }, 1000);
+        !(params.id == image.name) && setShow(false)
+
+        return () => clearTimeout(timeoutId);
+    }, [params]);
+
+
     const { nodes } = useGLTF('./model//WRoom/WRoom.glb')
     console.log(nodes);
 
@@ -47,27 +66,20 @@ export default function WRoom() {
                 transform
                 wrapperClass='htmlScreen'
                 distanceFactor={1.17}
-                position={[position.x, position.y, 0.5]}
+                position={image.position}
             >
-                <iframe
-                    style={{
-                        width: 1048 + scale.x,
-                        height: 670 + scale.y,
-                    }}
-                    src='https://fluid-threejs.netlify.app/' />
+                <div className={`zoom-in-out-fade-example ${show ? 'show' : 'hide'}`}>
+
+                    <iframe
+                        style={{
+                            width: 1048 + image.args[0],
+                            height: 670 + image.args[1],
+                        }}
+                        src='https://fluid-threejs.netlify.app/' />
+                </div>
             </Html>
             <meshBasicMaterial transparent opacity={0} />
         </mesh>
-
-
-
-        {/* <Sparkles
-                size={5}
-                scale={[4, 2, 4]}
-                position-y={1}
-                speed={0.5}
-                count={40}
-            /> */}
 
         {/* </Center> */}
     </>
