d487d0302a576b7ec1bcddadc8c37bfd64cbed67
Ahmed Abdien <ahmed.g.abdien@gmail.com>
2023-06-21 15:26:53 +0300

    Kitchen-upload by MassageBubble

--- src/components/Rooms/Kitchen.jsx
+++ src/components/Rooms/Kitchen.jsx
@@ -20,9 +20,9 @@ const images = [
 ]
 
 const massages = [
-    { name: RoomName + "ContentImage", rotation: [-0.27, 0.34, 0.09], position: [-1.33, 1.42, 0.94], args: [0.862, 0.856], defaultImageURL: baseURL + '/content.jpg' },
+    { name: RoomName + "ContentImage", rotation: [-0.27, 0.34, 0.09], position: [-1.33, 1.42, 0.94], args: [0.862, 0.856], defaultImageURL: baseURL + '/content.jpg', left: true },
 
-    { name: RoomName + "StyleImage", rotation: [-0.28, -0.455, -0.128], position: [1.436, 1.43, 1.02], args: [0.862, 0.856], defaultImageURL: baseURL + '/style.jpg' }
+    { name: RoomName + "StyleImage", rotation: [-0.28, -0.455, -0.128], position: [1.436, 1.43, 1.02], args: [0.862, 0.856], defaultImageURL: baseURL + '/style.jpg', left: false }
 ]
 
 const buttonFrames = [
@@ -58,8 +58,8 @@ export default function Kitchen({ userId, setIsContentImageUploaded, setIsStyleI
 
     images[0]['imageUrl'] = contentImageUrl
     images[1]['imageUrl'] = styleImageUrl
-    buttonFrames[0]['setImageUrl'] = setContentImageUrl
-    buttonFrames[1]['setImageUrl'] = setStyleImageUrl
+    massages[0]['setImageUrl'] = setContentImageUrl
+    massages[1]['setImageUrl'] = setStyleImageUrl
 
 
     /**
@@ -88,8 +88,9 @@ export default function Kitchen({ userId, setIsContentImageUploaded, setIsStyleI
             </mesh>
 
             <Frames name={RoomName} activeRoomName={activeRoomName} Children={Frame} bigImageFocus={bigImageFocus} basePOV={basePOV} images={images} />
-            <RightMassageBubble {...massages[0]} />
-            <LeftMassageBubble {...massages[1]} />
+            {/* <MassageBubble {...massages[0]} />
+            <MassageBubble {...massages[1]} /> */}
+            {massages.map((props, i) => userId && <MassageBubble key={i} userId={userId} {...props} />)}
             {/* {buttonFrames.map((props, i) => userId && <ButtonFrame key={i} userId={userId} {...props} />)} */}
         </group>
     </>
@@ -115,12 +116,34 @@ const Frame = ({ imageUrl, name, position, rotation, args, defaultImageURL }) =>
 
 
 
-const LeftMassageBubble = ({ name, position, rotation }) => {
+const MassageBubble = ({ userId, name, position, rotation, left, setImageUrl }) => {
 
     const [show, setShow] = useState(false);
     const [, params] = useRoute('/:id')
-    const isActive = true
-    // const isActive = params?.id == name
+    // const isActive = true
+    const isActive = params?.id == name
+
+    const cloudinaryRef = useRef()
+    const widgetRef = useRef()
+
+
+    const handleImageUpload = (_, result) => {
+
+        if (result && result.event === 'success') {
+            setImageUrl(result.info.url);
+        }
+    };
+
+    useEffect(() => {
+        cloudinaryRef.current = window.cloudinary
+        widgetRef.current = cloudinaryRef.current.createUploadWidget({
+            cloudName: "dcmthd8bn",
+            uploadPreset: "utmds9zl",
+            publicId: name + userId,
+            sources: ['local', 'url', 'image_search'],
+            resourceType: 'image'
+        }, handleImageUpload)
+    }, [])
 
     useEffect(() => {
         const timeoutId = setTimeout(() => {
@@ -140,45 +163,11 @@ const LeftMassageBubble = ({ name, position, rotation }) => {
             position={position}
             rotation={rotation}
         >
-            <div onClick={(e) => (e.stopPropagation(), console.log("hi"))} className={`left-top zoom-in-out-fade-Kitchen ${show ? 'show' : 'hide'}`}>
-                <div className="talk-bubble-kitchen tri-left round left-top">
-                    <div className="talktext">
-                        <ol>
-                            Upload
-                        </ol>
-                    </div>
-                </div>
-            </div>
-        </Html >
-    </>
-}
-const RightMassageBubble = ({ name, position, rotation }) => {
-
-    const [show, setShow] = useState(false);
-    const [, params] = useRoute('/:id')
-    const isActive = true
-    // const isActive = params?.id == name
-
-    useEffect(() => {
-        const timeoutId = setTimeout(() => {
-            isActive && setShow(true);
-        }, 1000);
-        !isActive && setShow(false)
-
-        return () => clearTimeout(timeoutId);
-    }, [params]);
-
-
-    return <>
-        <Html
-            transform
-            scale={0.5}
-            distanceFactor={1.17}
-            position={position}
-            rotation={rotation}
-        >
-            <div onClick={(e) => (e.stopPropagation(), console.log("hi"))} className={`zoom-in-out-fade-example ${show ? 'show' : 'hide'}`}>
-                <div className="talk-bubble-kitchen tri-right round right-top">
+            <div
+                onClick={(e) => (e.stopPropagation(), widgetRef.current.open())}
+                className={`${!left ? 'left-top-origin' : 'right-top-origin'} zoom-in-out-fade-Kitchen ${show ? 'show' : 'hide'}`}
+            >
+                <div className={`talk-bubble-kitchen round ${!left ? 'tri-left left-top' : 'tri-right right-top'}`}>
                     <div className="talktext">
                         <ol>
                             Upload
