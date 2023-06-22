1d308dfeacdb27988d947a1e832924b43d168d6c
Ahmed Abdien <ahmed.g.abdien@gmail.com>
2023-06-22 17:16:35 +0300

    Overlay-+overlay

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 // import App from './App'
 // import CheckResources from './CheckResources'
 import Experience from './Experience'
+import { Overlay } from './Overlay'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 root.render(
     <>
         {/* <App /> */}
         <Experience />
+        <Overlay />
     </>
 )
