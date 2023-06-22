e555d4a8e45fceb48b2336c26e38d374a6aa07ff
Ahmed Abdien <ahmed.g.abdien@gmail.com>
2023-06-22 15:55:39 +0300

    Arrows-enhance shape

--- src/components/Arrows.jsx
+++ src/components/Arrows.jsx
@@ -1,34 +1,30 @@
-import { Text } from "@react-three/drei"
+import { Html, Text } from "@react-three/drei"
 import { useThree } from "@react-three/fiber"
 
 export const Arrows = ({ rightAction, leftAction }) => {
-    const arrow = ">"
-    const { width, height } = useThree(state => state.viewport)
-    const x = width - Math.max(2, width * 0.8)
-    const y = -height + height
-    // const rightArrowPosition = [x, y, 0]
-    const rightArrowPosition = [1.1666699993484134, 0, -5]
-    // const leftArrowPosition = [-x, y, 0]
-    const leftArrowPosition = [-1.1666699993484134, 0, -5]
+    const rightArrowPosition = [1.7, 0, -5]
+    const leftArrowPosition = [-1.7, 0, -5]
     return <>
-        <group>
-            <Text
-                color="red"
-                position={rightArrowPosition}
-                scale={0.25}
-                onClick={rightAction} >
-                {arrow}
-            </Text>
-        </group>
-        <group>
-            <Text
-                rotation-y={Math.PI}
-                color="red"
-                position={leftArrowPosition}
-                scale={0.25}
-                onClick={leftAction} >
-                {arrow}
-            </Text>
-        </group>
+        <Arrow text={"NEXT.."} arrowPosition={rightArrowPosition} action={rightAction} right />
+        <Arrow text={"..BACK"} arrowPosition={leftArrowPosition} action={leftAction} />
+    </>
+}
+
+const Arrow = ({ text, arrowPosition, action, right = false }) => {
+    return <>
+        <Html
+            transform
+            scale={1}
+            distanceFactor={1.17}
+            position={arrowPosition}
+            className={`arrow hover-underline-animation ${right ? "left-to-right" : "right-to-left"}`}
+        >
+
+            <div
+                className={`arrow hover-underline-animation2 ${right ? "right-vertical" : "left-vertical"}`}
+                onClick={action}>
+                {text}
+            </div>
+        </Html >
     </>
 }
